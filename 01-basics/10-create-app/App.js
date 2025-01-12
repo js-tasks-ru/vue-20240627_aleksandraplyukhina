4c2a0741ebcdd18704d7f0fd65311ebf007554e2
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2025-01-12 13:14:03 +0300

    update create-app task

--- 01-basics/10-create-app/App.js
+++ 01-basics/10-create-app/App.js
@@ -3,19 +3,16 @@ import { defineComponent, computed } from 'vue'
 export default defineComponent({
   name: 'App',
 
-  // setup() {
-  //   const date = new Date();
-  //   const formattedDate = computed(() => {
-  //     return new Intl.DateTimeFormat('ru-RU', {dateStyle: 'long'}).format(date)
-  //   })
-  //
-  //   return {
-  //     formattedDate,
-  //   }
-  // },
-  //
-  // template: `
-  //   <div>Сегодня {{ formattedDate }}</div>
-  // `,
-  template: `Сегодня ${new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })}`,
+  setup() {
+    const date = new Date();
+    const formattedDate = new Intl.DateTimeFormat(navigator.language, {dateStyle: 'long'}).format(date)
+
+    return {
+      formattedDate,
+    }
+  },
+
+  template: `
+    <div>Сегодня {{ formattedDate }}</div>
+  `,
 })
